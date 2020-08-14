ce9fdfd15346596dd9acf9e52a7da77710a18754
mjbryan <mjbyran10@gmail.com>
2020-08-14 13:07:57 +0200

    Added mock data interfaces and classes

---
+++ BethanysPieShop/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BethanysPieShop.Models
+{
+    public class Category
+    {
+        public int CategoryId { get; internal set; }
+        public string CategoryName { get; internal set; }
+        public string Description { get; internal set; }
+    }
+}
