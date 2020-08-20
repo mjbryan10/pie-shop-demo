f1931f15ddc93ec902464c4d4d1e94f5c4020c28
mjbryan <mjbyran10@gmail.com>
2020-08-20 10:33:45 +0200

    Added Pie and Category Repositories

---
+++ BethanysPieShop/Models/CategoryRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BethanysPieShop.Models
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public CategoryRepository(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        IEnumerable<Category> ICategoryRepository.AllCategories => _appDbContext.Categories;
+    }
+}
