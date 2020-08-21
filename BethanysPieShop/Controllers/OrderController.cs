d5320ecd3b47ca97607b99db7de28025cbf782d2
mjbryan <mjbyran10@gmail.com>
2020-08-21 16:26:17 +0200

    Fixed dependancy injection on OrderController

--- BethanysPieShop/Controllers/OrderController.cs
+++ BethanysPieShop/Controllers/OrderController.cs
@@ -13,7 +13,7 @@ namespace BethanysPieShop.Controllers
         private readonly IOrderRepository _orderRepository;
         private readonly ShoppingCart _shoppingCart;
 
-        public OrderController(OrderRepository orderRepository, ShoppingCart shoppingCart)
+        public OrderController(IOrderRepository orderRepository, ShoppingCart shoppingCart)
         {
             _orderRepository = orderRepository;
             _shoppingCart = shoppingCart;
